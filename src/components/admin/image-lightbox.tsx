184bb12bc3d27e8024c6cc958aeec85ffbcc6c77
WRTeamYash <yashwrteam89@gmail.com>
2026-08-11 20:13:35 +0530

    fix: lightbox frame is a fixed square, image letterboxed inside via object-contain

--- src/components/admin/image-lightbox.tsx
+++ src/components/admin/image-lightbox.tsx
@@ -32,9 +32,9 @@ export function ImageLightboxTrigger({
 
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent className="w-auto max-w-[calc(100%-2rem)] bg-transparent p-0 shadow-none ring-0 sm:max-w-[calc(100%-2rem)]">
-          <div className="flex items-center justify-center rounded-xl bg-card p-6 ring-1 ring-foreground/10">
+          <div className="flex size-[min(80vw,80vh,480px)] items-center justify-center rounded-xl bg-card p-8 ring-1 ring-foreground/10">
             {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={src} alt={alt} className="max-h-[70vh] max-w-[80vw] w-auto h-auto object-contain" />
+            <img src={src} alt={alt} className="max-h-full max-w-full object-contain" />
           </div>
         </DialogContent>
       </Dialog>
